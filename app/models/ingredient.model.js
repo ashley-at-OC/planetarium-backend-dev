3f49a9fc52c6ddfa6e4f25d49bc33ee18f3a46f4
Ashley <ashphamley@gmail.com>
2026-06-05 15:08:46 -0500

    Ingredient model update

--- app/models/ingredient.model.js
+++ app/models/ingredient.model.js
@@ -1,16 +1,40 @@
 module.exports = (sequelize, Sequelize) => {
   const Ingredient = sequelize.define("ingredient", {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
     name: {
       type: Sequelize.STRING,
       allowNull: false,
     },
-    unit: {
-      type: Sequelize.STRING,
+    description: {
+      type: Sequelize.TEXT, // for longer strings
       allowNull: false,
     },
-    pricePerUnit: {
-      type: Sequelize.DECIMAL(10, 2),
+    price: {
+      type: Sequelize.DECIMAL,
+      allowNull: false,
     },
+    image: {
+      type: Sequelize.LONGBLOB, // to test without images
+      allowNull: true,
+    },
+    duration: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+
   });
   return Ingredient;
 };
