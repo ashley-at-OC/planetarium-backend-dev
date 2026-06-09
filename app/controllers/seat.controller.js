ee35c4c6dad9e8e8c046c80c79bc6fd85036b51a
Lance Hutter <lance.hutter04@gmail.com>
2026-06-08 22:04:53 -0500

    Add isBooked flag to GET /seats so the seat map can show reserved seats

--- app/controllers/seat.controller.js
+++ app/controllers/seat.controller.js
@@ -1,17 +1,32 @@
 const db = require("../models");
 const Seat = db.seat;
+const Ticket = db.ticket;
+const { Op } = db.Sequelize;
 
 // GET /recipeapi/seats
-// Fetch all seats, sorted for seat-map rendering.
 exports.findAll = async (req, res) => {
   try {
-    const data = await Seat.findAll({
+    const seats = await Seat.findAll({
       order: [
         ["seatRow", "ASC"],
         ["seatColumn", "ASC"],
       ],
     });
-    res.send(data);
+
+    // Pull just the seatIds of any active tickets, so we can flag those seats.
+    const bookedTickets = await Ticket.findAll({
+      where: { ticketStatus: { [Op.in]: ["valid", "used"] } },
+      attributes: ["seatId"],
+    });
+    const bookedSeatIds = new Set(bookedTickets.map((t) => t.seatId));
+
+    // Attach the flag to each seat record.
+    const result = seats.map((s) => ({
+      ...s.toJSON(),
+      isBooked: bookedSeatIds.has(s.id), 
+    }));// the seat has a ticket with status "valid" or "used""
+
+    res.send(result);
   } catch (err) {
     res.status(500).send({
       message: err.message || "Some error occurred while retrieving seats.",
