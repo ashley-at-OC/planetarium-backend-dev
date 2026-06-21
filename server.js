de38c418573ee6a5fe7e3c61a39d3645095049cb
kixu <codewithkixu@gmail.com>
2026-06-20 19:49:59 -0500

    add aws api to listen to frontend

--- server.js
+++ server.js
@@ -4,8 +4,50 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 const db = require("./app/models");
+
+const PORT = process.env.PORT || 3200;
+
+const allowedOrigins = [
+  "http://localhost:8081",
+  "http://localhost:5173",
+  "http://ec2-54-227-117-253.compute-1.amazonaws.com",
+  "http://ec2-54-227-117-253.compute-1.amazonaws.com:3200",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the planetarium backend." });
+});
+
+require("./app/routes/auth.routes.js")(app);
+require("./app/routes/show.routes")(app);
+require("./app/routes/recipe.routes")(app);
+require("./app/routes/recipeStep.routes")(app);
+require("./app/routes/recipeShow.routes")(app);
+require("./app/routes/seat.routes")(app);
+require("./app/routes/user.routes")(app);
+require("./app/routes/showtime.routes.js")(app);
+require("./app/routes/booking.routes.js")(app);
+require("./app/routes/payment.routes.js")(app);
+require("./app/routes/ticket.routes.js")(app);
 
 const startServer = async () => {
   try {
@@ -13,7 +55,7 @@ const startServer = async () => {
     console.log("Database synced.");
 
     if (process.env.NODE_ENV !== "test") {
-      app.listen(PORT, () => {
+      app.listen(PORT, "0.0.0.0", () => {
         console.log(`Server is running on port ${PORT}.`);
       });
     }
@@ -23,39 +65,6 @@ const startServer = async () => {
   }
 };
 
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
-
-app.use(cors(corsOptions));
-app.options("*", cors());
-
-// parse requests of content-type - application/json
-app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the recipe backend." });
-});
-
-require("./app/routes/auth.routes.js")(app);
-require("./app/routes/show.routes")(app);
-require("./app/routes/recipe.routes")(app);
-require("./app/routes/recipeStep.routes")(app);
-require("./app/routes/recipeShow.routes")(app);
-require("./app/routes/seat.routes")(app); // GET /planetariumapi/seats
-require("./app/routes/user.routes")(app);
-require("./app/routes/showtime.routes.js")(app);
-require("./app/routes/booking.routes.js")(app);
-require("./app/routes/payment.routes.js")(app);
-require("./app/routes/ticket.routes.js")(app);
-
-
-// set port, listen for requests
-const PORT = process.env.PORT || 3200;
 startServer();
 
 module.exports = app;
